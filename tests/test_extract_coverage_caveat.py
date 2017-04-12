329060bd037d772bcab87a3884d133141e19281a
Thomas Whitington <thomaswhitington@gmail.com>
2017-04-12 15:57:07 +0200

    Fixing parsing issues for coverage and contamination data files.

--- tests/test_extract_coverage_caveat.py
+++ tests/test_extract_coverage_caveat.py
@@ -1,46 +1,58 @@
 import unittest
-import pandas as pd
+from mock import mock_open, patch
 
 from extract_coverage_caveat import extract_qc_call
 
 
 class TestMethods(unittest.TestCase):
     def test_extract_coverage_caveat_ok1(self):
-        data = [[110, 3, 548, 0.04],
-                [111, 7, 548, 0.21],
-                [112, 7, 548, 0.25],
-                [113, 10, 548, 0.25],
-                [120, 7, 548, 0.25]]
-        test_table = pd.DataFrame(data)
-
-        self.assertEquals(extract_qc_call(test_table, 0.95, 100, 0.95, 50), "OK")
+        with patch('extract_coverage_caveat.extract_qc_call.open',
+                   mock_open(read_data=
+                             """"# target_coverage_histogram, bam: path.bam
+# coverage\tbases_at_coverage\ttotal_bases\tfraction_bases_at_coverage
+110\t3\t548\t0.04
+111\t7\t548\t0.21
+112\t7\t548\t0.25
+113\t10\t548\t0.25
+120\t7\t548\t0.25"""),
+                   create=True) as test_input_file:
+        self.assertEquals(extract_qc_call(test_input_file, 0.95, 100, 0.95, 50), "OK")
 
     def test_extract_coverage_caveat_ok2(self):
-        data = [[40, 3, 548, 0.04],
-                [101, 7, 548, 0.21],
-                [112, 7, 548, 0.25],
-                [113, 10, 548, 0.25],
-                [120, 7, 548, 0.25]]
-        test_table = pd.DataFrame(data)
-
-        self.assertEquals(extract_qc_call(test_table, 0.95, 100, 0.95, 50), "OK")
+        with patch('extract_coverage_caveat.extract_qc_call.open',
+                   mock_open(read_data=
+                             """"# target_coverage_histogram, bam: path.bam
+# coverage\tbases_at_coverage\ttotal_bases\tfraction_bases_at_coverage
+40\t3\t548\t0.04
+101\t7\t548\t0.21
+112\t7\t548\t0.25
+113\t10\t548\t0.25
+120\t7\t548\t0.25"""),
+                   create=True) as test_input_file:
+            self.assertEquals(extract_qc_call(test_input_file, 0.95, 100, 0.95, 50), "OK")
 
     def test_extract_coverage_caveat_warn1(self):
-        data = [[40, 3, 548, 0.04],
-                [80, 7, 548, 0.21],
-                [112, 7, 548, 0.25],
-                [113, 10, 548, 0.25],
-                [120, 7, 548, 0.25]]
-        test_table = pd.DataFrame(data)
-
-        self.assertEquals(extract_qc_call(test_table, 0.95, 100, 0.95, 50), "WARN")
+        with patch('extract_coverage_caveat.extract_qc_call.open',
+                   mock_open(read_data=
+                             """"# target_coverage_histogram, bam: path.bam
+# coverage\tbases_at_coverage\ttotal_bases\tfraction_bases_at_coverage
+40\t3\t548\t0.04
+80\t7\t548\t0.21
+112\t7\t548\t0.25
+113\t10\t548\t0.25
+120\t7\t548\t0.25"""),
+                   create=True) as test_input_file:
+            self.assertEquals(extract_qc_call(test_input_file, 0.95, 100, 0.95, 50), "WARN")
 
     def test_extract_coverage_caveat_fail1(self):
-        data = [[40, 3, 548, 0.04],
-                [45, 7, 548, 0.21],
-                [112, 7, 548, 0.25],
-                [113, 10, 548, 0.25],
-                [120, 7, 548, 0.25]]
-        test_table = pd.DataFrame(data)
-
-        self.assertEquals(extract_qc_call(test_table, 0.95, 100, 0.95, 50), "FAIL")
+        with patch('extract_coverage_caveat.extract_qc_call.open',
+                   mock_open(read_data=
+                             """"# target_coverage_histogram, bam: path.bam
+# coverage\tbases_at_coverage\ttotal_bases\tfraction_bases_at_coverage
+40\t3\t548\t0.04
+45\t7\t548\t0.21
+112\t7\t548\t0.25
+113\t10\t548\t0.25
+120\t7\t548\t0.25"""),
+                   create=True) as test_input_file:
+            self.assertEquals(extract_qc_call(test_input_file, 0.95, 100, 0.95, 50), "FAIL")
